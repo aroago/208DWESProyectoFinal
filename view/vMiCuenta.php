8fbc2e98ea2870e1d388df9b23c16e24fca92ae3
aroa granero <aroa.graoma@educa.jcyl.es>
2022-01-27 13:38:03 +0100

    27/01/2022
    add imagen js

--- view/vMiCuenta.php
+++ view/vMiCuenta.php
@@ -48,43 +48,46 @@
                     <div class="row">
                         <div class="col-3 col-6-medium col-12-small">
                             <div id="containerRegistro">
-                                    <form name="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data" >
-                                        <h3>Usuario: </h3>
-                                        <input  type="text" name="CodUsuario" value="<?php echo $aVMiCuenta['CodUsuario'] ?>" disabled/>
-                                        <br>
+                                <form name="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data" >
+                                    <h3>Usuario: </h3>
+                                    <input  type="text" name="CodUsuario" value="<?php echo $aVMiCuenta['CodUsuario'] ?>" disabled/>
+                                    <br>
 
-                                        <h3>Descripción de usuario: </h3>
-                                        <input  type="text" name="DescUsuario" value="<?php
-                                            echo $aVMiCuenta['DescUsuario']; 
-                                        ?>">
-                                        <span style="color:red">
-                                            <?php
-                                            if ($aErrores["DescUsuario"] != null) { //Compruebo que el array de errores no está vacío.
-                                                echo $aErrores["DescUsuario"]; //Si hay errores, devuelve el campo vacío y muestra una advertencia de los errores y como tiene que estar escrito ese campo.
-                                            }
-                                            ?>
-                                        </span>
+                                    <h3>Descripción de usuario: </h3>
+                                    <input  type="text" name="DescUsuario" value="<?php
+                                    echo $aVMiCuenta['DescUsuario'];
+                                    ?>">
+                                    <span style="color:red">
+                                        <?php
+                                        if ($aErrores["DescUsuario"] != null) { //Compruebo que el array de errores no está vacío.
+                                            echo $aErrores["DescUsuario"]; //Si hay errores, devuelve el campo vacío y muestra una advertencia de los errores y como tiene que estar escrito ese campo.
+                                        }
+                                        ?>
+                                    </span>
 
-                                        <br>
+                                    <br>
 
-                                        <h3>Fecha y Hora Ultima Conexion: </h3>
-                                        <input  type="fechaHoraUltimaConexion" name="fechaHoraUltimaConexion" value="<?php echo $aVMiCuenta['fechaHoraUltimaConexion'] ?>" disabled/>
-                                       
-                                        <br>
-                                        
-                                        <h3>Contraseña: </h3>
-                                        <input  type="password" name="cambiarPassword" value="<?php echo $aVMiCuenta['password'] ?>" disabled/>
-                                        <input type="submit" name="cambiarPassword" class="btnlogin" value="Cambiar Contraseña">
-                                        <br>
-                                        
-                                        <h3>Imagen: </h3>
-                                        <input name="archivo" id="archivo" type="file"/>
-                                        <br>
-                                        
-                                        <input type="submit" name="btnMiCuenta" class="btnlogin" value="ACEPTAR">
-                                        <input style="background: rgba(255, 3, 3, 0.3);" type="submit" name="borrarCuenta" class="btnlogin" value="Eliminar Cuenta">
-                                    </form>
-                                </div>
+                                    <h3>Fecha y Hora Ultima Conexion: </h3>
+                                    <input  type="fechaHoraUltimaConexion" name="fechaHoraUltimaConexion" value="<?php echo $aVMiCuenta['fechaHoraUltimaConexion'] ?>" disabled/>
+
+                                    <br>
+
+                                    <h3>Contraseña: </h3>
+                                    <input  type="password" name="cambiarPassword" value="<?php echo $aVMiCuenta['password'] ?>" disabled/>
+                                    <input type="submit" name="cambiarPassword" class="btnlogin" value="Cambiar Contraseña">
+                                    <br>
+
+                                    <h3>Imagen: </h3>
+                                    <input name="archivo" id="archivo" type="file"/>
+                                    <br>
+
+                                    <input type="submit" name="btnMiCuenta" class="btnlogin" value="ACEPTAR">
+                                    <input style="background: rgba(255, 3, 3, 0.3);" type="submit" name="borrarCuenta" class="btnlogin" value="Eliminar Cuenta">
+                                </form>
+                                <!-- La imagen que vamos a usar para previsualizar lo que el usuario selecciona -->
+                                <img id="imagenPrevisualizacion">
+                                <script src="./webroot/js/imagenUsuariojs.js"></script>
+                            </div>
                         </div>
                     </div>
                 </div>
